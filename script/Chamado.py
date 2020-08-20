b271fee6870b00f66b7f364035ef415057d59a93
ANDRE LARA <ANDRE_LARA@jabil.com>
2020-08-20 20:20:21 -0300

    error ident

--- script/Chamado.py
+++ script/Chamado.py
@@ -1,4 +1,4 @@
-    from PyQt5 import QtWidgets, QtCore, QtGui
+from PyQt5 import QtWidgets, QtCore, QtGui
 from PyQt5.QtWidgets import QApplication,QMainWindow
 from PyQt5.QtCore import QThread
 import time
