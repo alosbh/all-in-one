f4fba24ef55c224e8bfc2983a9ac361cd0c905a0
JHONATAN LACERDA <JHONATAN_LACERDA@jabil.com>
2020-11-25 11:09:03 -0300

    handling

--- script/Announcements.py
+++ script/Announcements.py
@@ -4,13 +4,16 @@ import urllib.request
 
 class Announcements:
     def load_announcements_label(self):
-        # get_urls = 'http://brbelm0itqa01/AIOServiceSTG/Images5S/GetAll?query='
-        url1_pixmap = self.get_pixmap('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRjG1dKKvgTCV1DATkPJx2a_JWhl_rEIsLCqQ&usqp=CAU')
-        url1_pixmap = url1_pixmap.scaled(591, 311, QtCore.Qt.KeepAspectRatio)
-        self.lbl_value_announcement_01.setPixmap(url1_pixmap)
-        url2_pixmap = self.get_pixmap('http://brbelm0apps01/UserImage/1000013.jpg')
-        url2_pixmap = url2_pixmap.scaled(441, 311, QtCore.Qt.KeepAspectRatio)
-        self.lbl_value_announcement_02.setPixmap(url2_pixmap)
+        try:
+            url1_pixmap = self.get_pixmap('http://brbelm0itqa01/AIOServiceSTG/Images5S/Frame1.png')
+            # url1_pixmap = url1_pixmap.scaled(591, 311, QtCore.Qt.KeepAspectRatio)
+            self.lbl_value_announcement_01.setPixmap(url1_pixmap)
+            url2_pixmap = self.get_pixmap('http://brbelm0itqa01/AIOServiceSTG/Images5S/Frame2.png')
+            # url2_pixmap = url2_pixmap.scaled(441, 311, QtCore.Qt.KeepAspectRatio)
+            self.lbl_value_announcement_02.setPixmap(url2_pixmap)
+        except:
+            self.lbl_value_announcement_01.setVisible(False)
+            self.lbl_value_announcement_02.setVisible(False)
     
     def get_pixmap(self, url):
         data = urllib.request.urlopen(url).read()
