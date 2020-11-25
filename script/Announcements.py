68a9ca9e87f9ccca5f561c8ed59db7c91a0e43cc
JHONATAN LACERDA <JHONATAN_LACERDA@jabil.com>
2020-11-25 09:52:30 -0300

    mural pronto? covid

--- script/Announcements.py
+++ script/Announcements.py
@@ -5,7 +5,7 @@ import urllib.request
 class Announcements:
     def load_announcements_label(self):
         # get_urls = 'http://brbelm0itqa01/AIOServiceSTG/Images5S/GetAll?query='
-        url1_pixmap = self.get_pixmap('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRjG1dKKvgTCV1DATkPJx2a_JWhl_rEIsLCqQ&usqp=CAU')
+        url1_pixmap = self.get_pixmap('https://www.hollyhedge.com/wp-content/uploads/2020/03/COVID19_Info-1180x520-661x331.jpg')
         self.lbl_value_announcement_01.setPixmap(url1_pixmap)
         url2_pixmap = self.get_pixmap('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRjG1dKKvgTCV1DATkPJx2a_JWhl_rEIsLCqQ&usqp=CAU')
         self.lbl_value_announcement_02.setPixmap(url2_pixmap)
