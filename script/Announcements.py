8ad06bfac4caeb5975457f756731c20fe7c505bc
JHONATAN LACERDA <JHONATAN_LACERDA@jabil.com>
2020-11-25 13:56:45 -0300

    aoskdasd

--- script/Announcements.py
+++ script/Announcements.py
@@ -5,6 +5,7 @@ import urllib.request
 class Announcements:
     def load_announcements_label(self):
         try:
+            self.lbl_value_announcement_01.setVisible(True)
             url1_pixmap = self.get_pixmap('http://brbelm0itqa01/AIOServiceSTG/Images5S/Frame1.png')
             # url1_pixmap = url1_pixmap.scaled(591, 311, QtCore.Qt.KeepAspectRatio)
             self.lbl_value_announcement_01.setPixmap(url1_pixmap)
@@ -12,6 +13,7 @@ class Announcements:
             self.lbl_value_announcement_01.setVisible(False)
 
         try:
+            self.lbl_value_announcement_01.setVisible(True)
             url2_pixmap = self.get_pixmap('http://brbelm0itqa01/AIOServiceSTG/Images5S/Frame2.png')
             # url2_pixmap = url2_pixmap.scaled(441, 311, QtCore.Qt.KeepAspectRatio)
             self.lbl_value_announcement_02.setPixmap(url2_pixmap)
