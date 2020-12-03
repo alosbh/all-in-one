49e62f071b2b74fa71181bb3d46c381e38cee1fa
JHONATAN LACERDA <JHONATAN_LACERDA@jabil.com>
2020-12-03 13:24:47 -0300

    consertando lpa

--- script/LPAactions_controller.py
+++ script/LPAactions_controller.py
@@ -4,7 +4,10 @@ import requests
 import json
 
 class LPAactions_controller():
+    workstation_namePP = None
+
     def LPAactions_functions(self, workstation_name):
+        self.workstation_namePP = workstation_name
         url_LPAactions = 'http://brbelm0apps02/AIOService/Lpa/GetOpenActionsByPost/?parameters=' + workstation_name
         request_LPAactions = requests.get(url_LPAactions)
         response_LPAactions = request_LPAactions.json()
@@ -12,13 +15,14 @@ class LPAactions_controller():
             self.btn_actionsLPA.hide()
             self.lbl_value_number_actionsLPA.hide()
         else:
+            self.btn_actionsLPA.setEnabled(True)
             self.btn_actionsLPA.setVisible(True)
             self.lbl_value_number_actionsLPA.setVisible(True)
             self.lbl_value_number_actionsLPA.setText(str(response_LPAactions))
-            self.btn_actionsLPA.clicked.connect(lambda: self.redirect_LPAactions(workstation_name))
+            self.btn_actionsLPA.clicked.connect(self.redirect_LPAactions)
 
 
-    def redirect_LPAactions(self, workstation_name):
-        url_LPAactions = 'http://brbelm0apps01/LPAEletronico/lpa/GetOpenActionsByPost/' + workstation_name
+    def redirect_LPAactions(self):
+        url_LPAactions = 'http://brbelm0apps01/LPAEletronico/lpa/GetOpenActionsByPost/' + self.workstation_namePP
         self.body_web.load(QUrl(url_LPAactions))
         self.body_web.setVisible(True)
