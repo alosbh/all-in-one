516cf8789230bcdb207ab0200979134c556146fc
JHONATAN LACERDA <JHONATAN_LACERDA@jabil.com>
2021-03-09 10:56:51 -0300

    rollback

--- script/Raspberry.py
+++ script/Raspberry.py
@@ -44,36 +44,23 @@ class Raspberry:
 
     def request_rasp_hostname(self):
         ip = os.environ.get('SYSCON_IP')
-        
+        url = "http://" + ip + "/api/v1.0/system/info"
         
         try:
-            url = "http://" + ip + "/api/v1.0/system/info"
             request = requests.get(url)
             if request.status_code == 200:
                 response = json.loads(request.content)
                 self.Name = response['hostname']
             else:
                 mypath = Path(__file__).absolute().parent
-                with open('/etc/hostname','r') as f:
+                with open('hostname.txt') as f:
                     lines = f.readlines()
-                    hostname = lines[0]
-                    
-                hostname = hostname.replace("\n","").replace("'","")
-                self.Name = hostname
-                print("#########")
-                print(hostname)
-                print("#########")
+                self.Name = lines[0]
         except:
             mypath = Path(__file__).absolute().parent
-            with open('/etc/hostname','r') as f:
+            with open('hostname.txt') as f:
                 lines = f.readlines()
-                hostname = lines[0]
-                
-            hostname = hostname.replace("\n","").replace("'","")
-            self.Name = hostname
-            print("#########")
-            print(hostname)
-            print("#########")
+            self.Name = lines[0]
             
         
     def GetSystemInfo(self):
